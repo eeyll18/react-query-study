520f35cc98c8f60040f4fb56d5bd876f0a0dd14e
eeyll18 <edaeylulgunay2@gmail.com>
2026-06-12 18:10:41 +0300

    refactor structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import PostList from "./components/PostList";
 
 function App() {
-
-  return <></>;
+  return (
+    <div>
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
