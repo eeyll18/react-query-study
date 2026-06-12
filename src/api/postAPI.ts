5eac0bf7c16fdf4d2209be19310eece19efcada2
eeyll18 <edaeylulgunay2@gmail.com>
2026-06-12 18:26:49 +0300

    add api service for creating new posts

--- src/api/postAPI.ts
+++ src/api/postAPI.ts
@@ -1,9 +1,22 @@
-import type { Post } from "../types/post.types";
+import type { NewPost, Post } from "../types/post.types";
 
 export const fetchPosts = async (): Promise<Post[]> => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!response.ok) {
     throw new Error("Error fetching posts");
   }
+  return response.json();
+};
+
+export const addPost = async (newPost: NewPost): Promise<Post> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(newPost),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  });
+  if (!response.ok) {
+    throw new Error("Error adding post");
+  }
+
   return response.json();
 };
