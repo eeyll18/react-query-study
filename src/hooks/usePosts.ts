ebac7dad8e15465b2f4ed6f05b5bf049b0618b7b
eeyll18 <edaeylulgunay2@gmail.com>
2026-06-12 18:27:00 +0300

    create useAddPost mutation hook

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,10 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchPosts } from "../api/postAPI";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { addPost, fetchPosts } from "../api/postAPI";
+import type { NewPost } from "../types/post.types";
 
 // get method
 export const useGetPosts = () => {
   return useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
+  });
+};
+
+// post method
+export const useAddPost = () => {
+  const queryClient = useQueryClient(); // cache'i yönetmek için
+
+  return useMutation({
+    mutationFn: (newPost: NewPost) => addPost(newPost),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] }); // post eklendikten sonra oto güncelleme ve cache silme
+    },
   });
 };
